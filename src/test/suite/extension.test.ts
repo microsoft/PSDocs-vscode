720fd8b6214839eb5791c19ca77a93b81929b5c2
Vic Perdana <7114832+vicperdana@users.noreply.github.com>
2021-10-01 22:31:59 +1000

    Preview updated tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -5,16 +5,4 @@ import * as assert from "assert";
 import * as vscode from "vscode";
 // import * as myExtension from '../../extension';
 
-suite("Extension Test Suite", () => {
-  test("Get extension info", () => {
-    Extension.ext.info
-      .then((info) => {
-        assert.strictEqual("preview", info.channel);
-        assert.strictEqual("0.0.1", info.version);
-        assert.strictEqual("viperdan.psdocs-vscode-preview", info.id);
-      })
-      .catch((reason) => {
-        assert.fail(`Failed to get extension info. ${reason}`);
-      });
-  });
-});
+// to be added
